e8b6beb09cd5acb662055b5742493fe936a17d19
Sharon Varghese <kmit.sharonvarghese@gmail.com>
2018-01-18 20:30:26 +0530

    Update coin.py

--- coin.py
+++ coin.py
@@ -1,15 +1,20 @@
-class Coin(object):
-    def __init__(self, message, signature):
-        self.message = message 
-        self.signature = signature
+from Crypto.Signature import PKCS1_PSS
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA
 
-    def verify_coin(self):
-        try: 
-        	goofy_public_key = RSA.importKey(open('goofypk.pem').read())
-     		h = SHA.new()
-			h.update(message)
-          verifier = PKCS1_PSS.new(goofy_public_key)
-			if verifier.verify(h, self.signature):
+
+class Coin(object):
+	def __init__(self, message, signature):
+		self.message = message
+		self.signature = signature
+
+	def verify_coin(self):
+		try:
+			goofy_public_key = RSA.importKey(open('goofypk.pem').read())
+			sha_hash = SHA.new()
+			sha_hash.update(self.message)
+			verifier = PKCS1_PSS.new(goofy_public_key)
+			if verifier.verify(sha_hash, self.signature):
 				print "The signature is authentic."
 			else:
 				print "The signature is not authentic."
